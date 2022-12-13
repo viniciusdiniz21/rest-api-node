671de0c029dac7633e8e42e272e32ee9be5c111e
Vinicius Diniz <90654350+viniciusdiniz21@users.noreply.github.com>
2022-12-13 16:54:12 -0300

    Delete build directory

--- build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Server_1 = require("./server/Server");
-Server_1.server.listen(process.env.PORT || 3333, () => console.log("Server is running!"));
