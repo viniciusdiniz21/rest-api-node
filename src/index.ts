9f6aa6abd2251eaaa65fdbdf438ad7f78afe2d71
viniciusdiniz21 <vinicius.diniz444@gmail.com>
2023-02-02 10:06:42 -0300

    AJustes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import { server } from "./server/Server";
+import { Knex } from "./server/database/knex";
 
-server.listen(process.env.PORT || 3333, () =>
-  console.log("Server is running!")
-);
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () =>
+    console.log("Server is running!")
+  );
+};
+
+if (process.env.IS_LOCALHOST !== "true") {
+  Knex.migrate
+    .latest()
+    .then(() => startServer())
+    .catch(console.log);
+} else {
+  startServer();
+}
